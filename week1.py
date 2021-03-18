5aaf5b2583527b07baf64adbdcc88bfb0e0170f3
Beso Tabatadze <besotabatadze84@gmail.com>
2021-03-18 12:01:06 +0400

    Lecture 1

---
+++ week1.py
@@ -0,0 +1,11 @@
+# ===Exercise1_2===
+#x = input("X->")
+#print(x)
+#print("X=",x)
+#print(x+"8")
+#print(int(x)+8)
+print("Exercise1_2")
+x = int(input("X->"))
+print(bin(x))
+
+#print(bin(int(input("Y->"))))
