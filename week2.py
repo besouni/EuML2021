e02d898db7dfd102ffdfb5f6a0b957eac2b04b0f
Beso Tabatadze <besotabatadze84@gmail.com>
2021-03-25 11:41:45 +0400

    Lecture2

---
+++ week2.py
@@ -0,0 +1,26 @@
+
+
+# ===Exercise 2_8====
+# print("Exercise 2_8")
+# def average(x, y):
+#     print((x+y)/2)
+# average(3, 4)
+# average(3.4, 4.9)
+# average(8, 9)
+
+# ===Exercise 2_6====
+# import random
+# print("Exercise 2_6")
+# for r in range(0, 10):
+#     print(random.randint(5, 10))
+    # print(random.uniform(5, 10))
+
+# ===Exercise 2_2====
+# print("Exercise 2_2")
+# x = float(input("X->"))
+# y = float(input("Y->"))
+# z = float(input("Z->"))
+# print(max(x, y, z))
+
+
+
