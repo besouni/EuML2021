f7e82cbcd9e923933746b53de576a982049b0909
Beso Tabatadze <besotabatadze84@gmail.com>
2021-04-15 10:32:04 +0400

    Week5

---
+++ week5.py
@@ -0,0 +1,12 @@
+import numpy as np
+from numpy import random
+
+arr1 = random.randint(100, size=(24))
+print(arr1)
+print("=====")
+arr2 = random.randint(100, size=(6, 4))
+print(arr2)
+print("=====")
+arr3 = random.randint(100, size=(2, 3, 4))
+print(arr3)
+
